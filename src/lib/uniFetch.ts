9e5544175c4dc1d8688cee6dbde4dd5a7a2f84e7
pankecai <pankecai@gmail.com>
2023-03-16 13:39:52 +0800

    add lost file

---
+++ src/lib/uniFetch.ts
@@ -0,0 +1,26 @@
+declare const uni: any
+interface FetchOptions extends Omit<RequestInit, 'body'> {
+  data?: Record<string, unknown>
+}
+
+export function fetch(input: RequestInfo, options: FetchOptions = {}): Promise<Response> {
+  const { data, ...restOptions } = options
+  console.log('uniFetch---postgres-Fetch---', input, options)
+  return new Promise((resolve, reject) => {
+    uni.request({
+      url: typeof input === 'string' ? input : input.url,
+      method: restOptions.method || 'GET',
+      data: data || {},
+      header: {
+        'Content-Type': 'application/json',
+        ...restOptions.headers,
+      },
+      success: (res: any) => {
+        resolve(new Response(res.data))
+      },
+      fail: (err: any) => {
+        reject(new Error(`Request failed: ${err}`))
+      },
+    })
+  })
+}
---
+++ src/lib/urlUtil.ts
@@ -0,0 +1,16 @@
+export const addSearchParamsByRegx = (url: string, params: Record<string, string>) => {
+  const queryString = Object.keys(params)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    .join('&')
+  const regex = /(\?.*)?$/
+  url = url.replace(regex, `?${queryString}`)
+}
+
+
+export const getHostName = (url: string) => {
+  const regex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+\.[^:\/\n]+)/g
+  const match = regex.exec(url)
+  return match && match[1]
+}
+
+export declare const uni: any
